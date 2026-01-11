b214ac66ed6507f4ff87f0e506df0130aefc0f83
Charles Giann Marcelo <cm122927@gmail.com>
2026-01-11 17:48:20 +0800

    Update test_lcd.py

--- c_test/test_lcd.py
+++ c_test/test_lcd.py
@@ -4,12 +4,9 @@ from RPLCD.i2c import CharLCD
 import time
 
 lcd = CharLCD(i2c_addr=0x27, port=1, cols=16, rows=2)  # Try 0x3f if no display
-
-lcd.clear()
+print("📟 TAMTAP I2C LCD TEST")
 lcd.write_string("TAMTAP LCD TEST")
 time.sleep(2)
-lcd.clear()
 lcd.write_string("LEDs+Buzzer OK!")
 time.sleep(2)
-lcd.clear()
 print("✅ LCD WORKING!")
